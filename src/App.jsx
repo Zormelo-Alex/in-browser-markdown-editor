cc85e1c138409f25dd1198db8bb6b987651ca4ae
Zormelo-Alex <alexzormelo9@gmail.com>
2024-05-30 22:56:46 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="h-screen w-full flex justify-center items-center bg-gray-200">
+      <div className=" capitalize">
+        DOMs markdown application
+      </div>
+    </div>
+  );
+}
+
+export default App;
